2b47f18daf60230e23817ee1931db0847ae32f76
Jordan Kay <jkay10100@gmail.com>
2020-11-11 14:34:25 +0000

    Updates

---
+++ precache-manifest.85560fb2328e166833cc639cfffbbaba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e50df6e4e86fbcf2089f4df074b8f1",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "bf513887636124f427a8",
+    "url": "/To-Do-List/static/css/main.a99200be.chunk.css"
+  },
+  {
+    "revision": "a1ce7cd0e42686eec195",
+    "url": "/To-Do-List/static/js/2.cb777765.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/To-Do-List/static/js/2.cb777765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf513887636124f427a8",
+    "url": "/To-Do-List/static/js/main.a05c03a5.chunk.js"
+  },
+  {
+    "revision": "e71b0fca7b414be357cd",
+    "url": "/To-Do-List/static/js/runtime-main.8e2d492d.js"
+  }
+]);
